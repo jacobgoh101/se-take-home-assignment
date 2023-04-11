e303bf4b553eba87b126431b6dbaa67241de0d89
Jacob Goh <jacobgoh101@gmail.com>
2023-04-11 15:30:36 +0800

    init empty project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script >
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+}
+</script>
+
+<template>
+  <div>
+    {{ counter }}
+    <br />
+    <button @click="counter++">
+      increment
+    </button>
+  </div>
+</template>
+
+<style scoped>
+</style>
